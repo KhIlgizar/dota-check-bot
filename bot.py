3a757999e7372bc91c812ab6fc2344078ceed173
Khamidulin Ilgizar <khamidulin.ilgizar@yandex.ru>
2024-07-17 22:01:59 +0300

    feat: add mentions mapper

--- bot.py
+++ bot.py
@@ -4,6 +4,7 @@ from discord.ext import commands, tasks
 from clients import OpenDotaClient
 from utils import generate_text
 from env import LOOP_TIMEOUT
+from db.users import USERS_DB
 
 intents = discord.Intents.default()
 intents.message_content = True
@@ -13,9 +14,11 @@ bot = commands.Bot(command_prefix="/", intents=intents)
 last_match_id = 1
 
 @bot.command()
-async def watch(ctx, dota_id: str):
-  await check_stat.start(ctx, dota_id)
-    
+async def watch(ctx, id: str):
+  if(id.startswith("<@")):
+    id = USERS_DB[id]
+  await check_stat.start(ctx, id)
+
 @tasks.loop(seconds=float(LOOP_TIMEOUT))
 async def check_stat(ctx, dota_id):
   global last_match_id
@@ -25,7 +28,7 @@ async def check_stat(ctx, dota_id):
   match_id = stats["match_id"]
   account = dota_client.get_account()
   text = generate_text(stats, account)
-  
+
   if(match_id != last_match_id):
     await ctx.send(text)
     last_match_id = match_id
--- utils.py
+++ utils.py
@@ -1,10 +1,17 @@
 import random
 
-def get_random_phrase_from_file(file_path):
+def get_random_phrase_from_file(file_path, kda):
   try:
     with open(file_path, 'r', encoding='utf-8') as file:
       phrases = file.readlines()
+      if(kda >= 2):
+        phrases = phrases[50:74]
+      elif(kda >= 1):
+        phrases = phrases[25:49]
+      elif(kda < 1):
+        phrases = phrases[0:24]
     return random.choice(phrases).strip()
+  
   except FileNotFoundError:
     return "Файл не найден."
   except Exception as e:
@@ -12,12 +19,13 @@ def get_random_phrase_from_file(file_path):
 
 def generate_text(stats, account):
   kills, deaths, assists = stats["kills"],stats["deaths"],stats["assists"]
+  kda = (int(kills) + int(assists)) / int(deaths)
   name = account["profile"]["personaname"]
-    
+
   if(is_win(stats)):
-    phrase = get_random_phrase_from_file("assets/phrases_good.txt")
+    phrase = get_random_phrase_from_file("assets/phrases_good.txt", kda)
   else:
-    phrase = get_random_phrase_from_file("assets/phrases_bad.txt")
+    phrase = get_random_phrase_from_file("assets/phrases_bad.txt", kda)
   return f"[{name}] {phrase} - {kills}/{deaths}/{assists}"
 
 def is_win(stats):
